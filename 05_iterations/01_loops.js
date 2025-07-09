10659ef5cf233a0840126987404de69ebfaff0b6
Keshav Kumar <48651103+ksv508@users.noreply.github.com>
2025-07-09 05:07:07 +0000

    dom

--- 05_iterations/01_loops.js
+++ 05_iterations/01_loops.js
@@ -1,133 +1,98 @@
-// for
-
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element == 5) {
-        //console.log("5 is best number");
-    }
-    //console.log(element);
+// //1st Class
+// // for
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         //console.log("5 is best number");
+//     }
+//     //console.log(element);
     
-}
+// }
 
-// console.log(element);
+// // console.log(element);
 
-for (let i = 1; i <= 10; i++) {
-    //console.log(`Outer loop value: ${i}`);
-   for (let j = 1; j <= 10; j++) {
-    //console.log(`Inner loop value ${j} and inner loop ${i}`);
-    //console.log(i + '*' + j + ' = ' + i*j );
-   }
+// for (let i = 1; i <= 10; i++) {
+//     //console.log(`Outer loop value: ${i}`);
+//    for (let j = 1; j <= 10; j++) {
+//     //console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     //console.log(i + '*' + j + ' = ' + i*j );
+//    }
     
-}
-let myArray = ["flash", "batman", "superman"]
-//console.log(myArray.length);
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    //console.log(element);
+// }
+// // let myArray = ["flash", "batman", "superman"]
+// //console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     //console.log(element);
     
-}
+// }
 
 
-// break and continue
+// // break and continue
+
+// // for (let index = 1; index <= 20; index++) {
+// //     if (index == 5) {
+// //         console.log(`Detected 5`);
+// //         break
+// //     }
+// //    console.log(`Value of i is ${index}`);
+    
+// // }
 
 // for (let index = 1; index <= 20; index++) {
 //     if (index == 5) {
 //         console.log(`Detected 5`);
-//         break
+//         continue
 //     }
 //    console.log(`Value of i is ${index}`);
     
 // }
 
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected 5`);
-        continue
-    }
-   console.log(`Value of i is ${index}`);
-    
-}
 
 
-let index = 0
-// while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
+// //2nd Class
+
+// let index = 0
+// // while (index <= 10) {
+// //     console.log(`Value of index is ${index}`);
+// //     index = index + 2
+// // }
+
+// let myArray = ['flash', "batman", "superman"]
+
+// // let arr = 0
+// while (arr < myArray.length) {
+//     //console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
 // }
 
-let myArray = ['flash', "batman", "superman"]
+// let score = 11
 
-let arr = 0
-while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
-    arr = arr + 1
-}
-
-let score = 11
-
-do {
-    console.log(`Score is ${score}`);
-    score++
-} while (score <= 10);
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
 
 
-// for of
 
-// ["", "", ""]
-// [{}, {}, {}]
+// //3rd Class
+// // for of
 
-const arr = [1, 2, 3, 4, 5]
+// // ["", "", ""]
+// // [{}, {}, {}]
 
-for (const num of arr) {
-    //console.log(num);
-}
+// const arr = [1, 2, 3, 4, 5]
 
-const greetings = "Hello world!"
-for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
-}
-
-// Maps
-
-const map = new Map()
-map.set('IN', "India")
-map.set('USA', "United States of America")
-map.set('Fr', "France")
-map.set('IN', "India")
-
-
-// console.log(map);
-
-for (const [key, value] of map) {
-    // console.log(key, ':-', value);
-}
-
-const myObject = {
-    game1: 'NFS',
-    game2: 'Spiderman'
-}
-
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
+// for (const num of arr) {
+//     //console.log(num);
 // }
 
-const myObject = {
-    js: 'javascript',
-    cpp: 'C++',
-    rb: "ruby",
-    swift: "swift by apple"
-}
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//     //console.log(`Each char is ${greet}`)
+// }
 
-for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
-}
-
-const programming = ["js", "rb", "py", "java", "cpp"]
-
-for (const key in programming) {
-    //console.log(programming[key]);
-}
+// // Maps
 
 // const map = new Map()
 // map.set('IN', "India")
@@ -135,138 +100,194 @@ for (const key in programming) {
 // map.set('Fr', "France")
 // map.set('IN', "India")
 
-// for (const key in map) {
-//     console.log(key);
+
+// // console.log(map);
+
+// for (const [key, value] of map) {
+//     // console.log(key, ':-', value);
 // }
 
-const coding = ["js", "ruby", "java", "python", "cpp"]
-
-// coding.forEach( function (val){
-//     console.log(val);
-// } )
-
-// coding.forEach( (item) => {
-//     console.log(item);
-// } )
-
-// function printMe(item){
-//     console.log(item);
+// const myObject = {
+//     game1: 'NFS',
+//     game2: 'Spiderman'
 // }
 
-// coding.forEach(printMe)
-
-// coding.forEach( (item, index, arr)=> {
-//     console.log(item, index, arr);
-// } )
-
-const myCoding = [
-    {
-        languageName: "javascript",
-        languageFileName: "js"
-    },
-    {
-        languageName: "java",
-        languageFileName: "java"
-    },
-    {
-        languageName: "python",
-        languageFileName: "py"
-    },
-]
-
-myCoding.forEach( (item) => {
+// // for (const [key, value] of myObject) {
+// //     console.log(key, ':-', value);
     
-    console.log(item.languageName);
-} )
+// // }
 
+
+
+// //4th Class
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+
+// for (const key in myObject) {
+//     //console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     //console.log(programming[key]);
+// }
+
+// // const map = new Map()
+// // map.set('IN', "India")
+// // map.set('USA', "United States of America")
+// // map.set('Fr', "France")
+// // map.set('IN', "India")
+
+// // for (const key in map) {
+// //     console.log(key);
+// // }
+
+
+
+
+// //5th Class
 // const coding = ["js", "ruby", "java", "python", "cpp"]
 
+// // coding.forEach( function (val){
+// //     console.log(val);
+// // } )
 
-// const values = coding.forEach( (item) => {
-//     //console.log(item);
-//     return item
+// // coding.forEach( (item) => {
+// //     console.log(item);
+// // } )
+
+// // function printMe(item){
+// //     console.log(item);
+// // }
+
+// // coding.forEach(printMe)
+
+// // coding.forEach( (item, index, arr)=> {
+// //     console.log(item, index, arr);
+// // } )
+
+// const myCoding = [
+//     {
+//         languageName: "javascript",
+//         languageFileName: "js"
+//     },
+//     {
+//         languageName: "java",
+//         languageFileName: "java"
+//     },
+//     {
+//         languageName: "python",
+//         languageFileName: "py"
+//     },
+// ]
+
+// myCoding.forEach( (item) => {
+    
+//     console.log(item.languageName);
 // } )
 
-// console.log(values);
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
-//     return num > 4
-// } )
+// //6th Class
+// // const coding = ["js", "ruby", "java", "python", "cpp"]
 
-// const newNums = []
 
-// myNums.forEach( (num) => {
-//     if (num > 4) {
-//         newNums.push(num)
-//     }
-// } )
+// // const values = coding.forEach( (item) => {
+// //     //console.log(item);
+// //     return item
+// // } )
+
+// // console.log(values);
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// // const newNums = myNums.filter( (num) => {
+// //     return num > 4
+// // } )
+
+// // const newNums = []
+
+// // myNums.forEach( (num) => {
+// //     if (num > 4) {
+// //         newNums.push(num)
+// //     }
+// // } )
+
+// // console.log(newNums);
+
+
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+//   ];
+
+//   let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+//   userBooks = books.filter( (bk) => { 
+//     return bk.publish >= 1995 && bk.genre === "History"
+// })
+//   console.log(userBooks);
+
+
+
+// //7th Class
+// const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// // const newNums = myNumers.map( (num) => { return num + 10})
+
+// const newNums = myNumers
+//                 .map((num) => num * 10 )
+//                 .map( (num) => num + 1)
+//                 .filter( (num) => num >= 40)
 
 // console.log(newNums);
 
 
-const books = [
-    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
-    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
-    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
-    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
-    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
-    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
-    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
-    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
-    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
-  ];
 
-  let userBooks = books.filter( (bk) => bk.genre === 'History')
+// //8th Class
+// const myNums = [1, 2, 3]
 
-  userBooks = books.filter( (bk) => { 
-    return bk.publish >= 1995 && bk.genre === "History"
-})
-  console.log(userBooks);
+// // const myTotal = myNums.reduce(function (acc, currval) {
+// //     console.log(`acc: ${acc} and currval: ${currval}`);
+// //     return acc + currval
+// // }, 0)
 
-  const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
-// const newNums = myNumers.map( (num) => { return num + 10})
-
-const newNums = myNumers
-                .map((num) => num * 10 )
-                .map( (num) => num + 1)
-                .filter( (num) => num >= 40)
-
-console.log(newNums);
-
-const myNums = [1, 2, 3]
-
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
-// }, 0)
-
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
-
-console.log(myTotal);
+// console.log(myTotal);
 
 
-const shoppingCart = [
-    {
-        itemName: "js course",
-        price: 2999
-    },
-    {
-        itemName: "py course",
-        price: 999
-    },
-    {
-        itemName: "mobile dev course",
-        price: 5999
-    },
-    {
-        itemName: "data science course",
-        price: 12999
-    },
-]
+// const shoppingCart = [
+//     {
+//         itemName: "js course",
+//         price: 2999
+//     },
+//     {
+//         itemName: "py course",
+//         price: 999
+//     },
+//     {
+//         itemName: "mobile dev course",
+//         price: 5999
+//     },
+//     {
+//         itemName: "data science course",
+//         price: 12999
+//     },
+// ]
 
-const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+// const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
-console.log(priceToPay);
+// console.log(priceToPay);
