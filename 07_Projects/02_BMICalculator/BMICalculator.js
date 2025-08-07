d69de80ce6fa3dc2601ad8a54493ed55020e407b
Keshav Kumar <48651103+ksv508@users.noreply.github.com>
2025-08-07 05:38:36 +0000

    2nd project completed

--- 07_Projects/02_BMICalculator/BMICalculator.js
+++ 07_Projects/02_BMICalculator/BMICalculator.js
@@ -1,12 +1,18 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit',function(action)){
+form.addEventListener('submit',function(action){
     action.preventDefault();
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
     const results = document.querySelector('#results');
     
-    if (height === '' || height < 0 || isNaN(height)) {
-        results.innerHTML("Please Enter Valid Height ${height}");
+    if (height === '' || height < 0 || isNaN(height))
+        results.innerHTML = `Please give a valid height ${height}`;
+    else if(weight === '' || weight < 0 || isNaN(weight))
+        results.innerHTML `Please Enter Valid Weight ${weight}`;
+    else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
     }
-};
+});
