00a6d20d0b7f2389b193d558111ae25e76eca3c3
Keshav Kumar <48651103+ksv508@users.noreply.github.com>
2025-08-07 03:40:11 +0000

    colorchanger done

--- 07_Projects/01_colorChanger/colorchanger.js
+++ 07_Projects/01_colorChanger/colorchanger.js
@@ -1,2 +1,15 @@
-const buttons = document.querySelectorAll('button')
-const body = document.querySelector('body')
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function (action){
+        if(action.target.id === "red")
+            body.style.backgroundColor = action.target.id;
+        if(action.target.id === "blue")
+            body.style.backgroundColor = action.target.id;
+        if(action.target.id === "green")
+            body.style.backgroundColor = action.target.id;
+        if(action.target.id === "gray")
+            body.style.backgroundColor = action.target.id;
+    });
+});
